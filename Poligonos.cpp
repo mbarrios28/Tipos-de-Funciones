071b3798c52a8b6445b9cb9c0bfd5b44b8a130ed
mbarrios28 <mbarriosmed@unsa.edu.pe>
2025-10-19 14:00:43 -0500

    Actualización en la función amiga

--- Poligonos.cpp
+++ Poligonos.cpp
@@ -39,7 +39,7 @@ class Punto {
 };
 
 double distancia (Punto& p1, Punto& p2){
-    return sqrt(pow(p1.getX()-p2.getX(),2) + pow(p1.getY()-p2.getY(),2));
+    return sqrt(pow(p1.x-p2.x,2) + pow(p1.y-p2.y,2));
 }
 class Poligono {
     private:
